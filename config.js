204d6d4978143c35b33ca4cd79377229c6d6cc25
aivo0 <aivo.olev@gmail.com>
2019-04-11 08:51:38 +0800

    Changed BE endpoint

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-export const endpoint = `http://localhost:4000`;
-export const prodEndpoint = `http://localhost:4000`;
+export const endpoint = `http://localhost:4000/`;
+export const prodEndpoint = `http://localhost:4000/`;
 //export const endpoint = `https://heli-yoga-prod.herokuapp.com/`;
 /* export const wssEndpoint = `ws://localhost:4000`;
 export const prodWssEndpoint = `wss://heli-yoga-prod.herokuapp.com/`; */
