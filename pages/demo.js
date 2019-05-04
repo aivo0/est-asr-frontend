15ae273da86f63b2d5ef99de162b68ed59ef2196
aivo0 <aivo.olev@gmail.com>
2019-05-05 07:50:06 +0800

    Removed survey

--- pages/demo.js
+++ pages/demo.js
@@ -11,34 +11,11 @@ const Inner = styled.div`
   @media (max-width: 450px) {
     padding: 10px;
   }
-  .my-survey {
-    margin-top: 0;
-    padding-top: 0;
-    margin-bottom: 10px;
-    display: flex;
-    justify-content: center;
-    p,
-    a {
-      font-size: 16px !important;
-    }
-    a {
-      border-bottom: 2px solid #fe621d;
-    }
-  }
 `;
 
 const Demo = props => (
   <>
     <Inner>
-      <div className="my-survey">
-        <p>
-          Palun aita kaasa mu magistritööle täites{" "}
-          <a href="https://www.surveymonkey.com/r/J379RKW" target="survey">
-            see 3-minutine küsimustik.
-          </a>{" "}
-          Tänan, Aivo Olev
-        </p>
-      </div>
       <DemoComponent
         id="5c9768e5857aba000713cde4"
         demoPeaks={demoPeaks}
--- pages/text.js
+++ pages/text.js
@@ -9,33 +9,10 @@ const Inner = styled.div`
   @media (max-width: 450px) {
     padding: 10px;
   }
-  .my-survey {
-    display: flex;
-    justify-content: center;
-    margin-top: 0;
-    padding-top: 0;
-    margin-bottom: 10px;
-    p,
-    a {
-      font-size: 16px !important;
-    }
-    a {
-      border-bottom: 2px solid #fe621d;
-    }
-  }
 `;
 
 const Text = props => (
   <Inner>
-    <div className="my-survey">
-      <p>
-        Palun aita kaasa mu magistritööle täites{" "}
-        <a href="https://www.surveymonkey.com/r/J379RKW" target="survey">
-          see 3-minutine küsimustik.
-        </a>{" "}
-        Tänan, Aivo Olev
-      </p>
-    </div>
     <Transcriber id={props.query.id} duration={props.query.duration} />
   </Inner>
 );
